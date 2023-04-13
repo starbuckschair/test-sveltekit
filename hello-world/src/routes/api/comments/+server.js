240e7bb81035ffecb26f857dd88ed973ade1e75a
서태원 <stainless@seotaewon-ui-MacBookPro.local>
2023-04-13 09:43:10 +0900

    API Post Request

---
+++ hello-world/src/routes/api/comments/+server.js
@@ -0,0 +1,17 @@
+import { json } from '@sveltejs/kit'
+import { comments } from "$lib/comments.js"
+
+export function GET() {
+    return json(comments)  
+}
+
+export async function POST(requestEvent) {
+    const {request } = requestEvent;
+    const { text } = await request.json()
+    const newComment = {
+        id: comments.length + 1,
+        text 
+    }
+    comments.push(newComment)
+    return json(newComment, { status: 201 })
+}

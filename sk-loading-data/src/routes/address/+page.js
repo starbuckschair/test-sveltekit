3bb951a38439f0989c602b6f748cc3929b2b4abb
서태원 <stainless@seotaewon-ui-MacBookPro.local>
2023-04-14 10:55:47 +0900

    Universal Load Function

--- sk-loading-data/src/routes/address/+page.js
+++ sk-loading-data/src/routes/address/+page.js
@@ -3,7 +3,7 @@ export const load = async (loadEvent) => {
     const response = await fetch('/api/postcodes');
     const postcodes = await response.json();
     const addressList = postcodes.map((postcode)=>{
-        return `${postcode.buildingName}, ${postcode.line1}, ${postcode.line3}`
+        return `${postcode.buildingName}, ${postcode.line1}, ${postcode.line3}, ${postcode.town}, ${postcode.country}, ${postcode.postcode}`
     });
     return { addressList}
 }

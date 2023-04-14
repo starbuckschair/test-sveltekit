f43225f143e7a9be72f67b9b94bfe51e6ff9216b
서태원 <stainless@seotaewon-ui-MacBookPro.local>
2023-04-14 09:16:53 +0900

    API Delete Request

--- hello-world/src/routes/api/comments/[commentId]/+server.js
+++ hello-world/src/routes/api/comments/[commentId]/+server.js
@@ -14,6 +14,14 @@ export async function PATCH(requestEvent){
     const { text } = await request.json();
     const comment = comments.find((comment)=> comment.id === parseInt(commentId));
     comment.text = text;
-    return json(comment);
-
+    return json(comment)
+}
+ 
+export async function DELETE(requestEvent) {
+    const { params } = requestEvent;
+    const { commentId } = params;
+    const deletedComment = comments.find((comment) => comment.id === parseInt(commentId));
+    const index = comments.findIndex((comment) => comment.id === parseInt(commentId));
+    comments.splice(index, 1);
+    return json(deletedComment);
 }
